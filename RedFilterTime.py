8fdbfc67fb40eb57f70c487d68afc5dd8f1592e7
ceberous <cerbus.collin@gmail.com>
2017-07-31 21:05:17 -0400

    Update RedFilterTime.py

--- RedFilterTime.py
+++ RedFilterTime.py
@@ -1,6 +1,11 @@
-import os , time , subprocess
+#!/usr/bin/python
+
+import sys , os , time , subprocess
+
+#https://github.com/shawnrice/alfred.flux
 
 wTimeZones = [ 'America/Los_Angeles' , 'America/Denver' , 'America/Detroit' , 'Europe/London' ]
+wBrazil = [ "-5.8" , "-35.2"  ]
 wAustralia = [ "-33.8" , "151.2" ]
 wSKorea = [ "37.5" , "127.0" ]
 XFLUX_CMD_BASE = "xflux -k 2000"
@@ -11,11 +16,18 @@ os.environ['TZ'] = wTimeZones[0]
 time.tzset()
 print time.strftime('%X %x %Z')
 
+wExtra = ""
+if ( len( sys.argv ) > 1 ): wExtra = sys.argv[1]
+
 cHour = int( time.strftime( '%H' ) )
-if ( cHour <= 18  ):
-	if ( cHour >= 4 ):
+if ( wExtra  == "revert" ):
+	XFLUX_CMD_FINAL = XFLUX_CMD_FINAL
+elif ( cHour <= 18  ):
+	if (cHour  >= 4 ):
+		XFLUX_CMD_FINAL = XFLUX_CMD_FINAL + " -l " + wBrazil[0] + " -g " + wBrazil[1]
+	else:
 		XFLUX_CMD_FINAL = XFLUX_CMD_FINAL + " -l " + wSKorea[0] + " -g " + wSKorea[1]
 
 print XFLUX_CMD_FINAL
-subprocess.call( "pkill -9 xflux" , shell=True )
+subprocess.call( "sudo pkill xflux" , shell=True )
 subprocess.call( XFLUX_CMD_FINAL , shell=True )
