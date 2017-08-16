39bd8b2b59fb9ff391b3c9fb30040d7862d50550
ceberous <cerbus.collin@gmail.com>
2017-08-16 00:30:27 -0700

    Update echoLocalRasPIS.py

--- echoLocalRasPIS.py
+++ echoLocalRasPIS.py
@@ -1,8 +1,18 @@
 import os
 import socket
+import fcntl
+import struct
+from subprocess import Popen, PIPE
 
-halo3PIStatus  = True if os.system("sudo ping -c 1 " + "halopi3.local") is 0 else False
+wIP = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8'$
+x1 = wIP.rfind( "." )
+wNet_Mask = wIP[ 0 : x1  ] + ".0/24"
+print wNet_Mask
 
-print "Local IP = " + ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
-
-print "halopi3 is Alive? = " + str(halo3PIStatus)
+wArgs = "sudo nmap -sP " + wNet_Mask + " | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'"
+print wArgs
+p = Popen( wArgs , shell=True , stdout=PIPE , bufsize=1 )
+with p.stdout:
+    for line in iter( p.stdout.readline , b'' ):
+        print line,
+p.wait()
