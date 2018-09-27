c72aeb9c66d5a7a618991f3974b5554ff4a65eb0
ceberous <cerbus.collin@gmail.com>
2018-09-26 21:20:13 -0700

    Update MoveAllSinksToSource0.js

--- MoveAllSinksToSource0.js
+++ MoveAllSinksToSource0.js
@@ -1,8 +1,12 @@
-#!/home/morpheous/.nvm/versions/node/v7.4.0/bin/node
-require('/home/morpheous/.nvm/versions/node/v7.4.0/lib/node_modules/shelljs/global');
+#!/bin/bash
+node -e 'const cp = require( "child_process" );
+const path = require( "path" ); 
+var groot = cp.spawnSync( "npm" , [ "root" , "-g" ] , { encoding : "utf8" } );
+groot = groot.stdout.trim();
+require( path.join( groot , "shelljs" , "global" ) );
 var setDefaultSink = "pacmd set-default-sink 0";
 exec( setDefaultSink , { silent:true , async:false } );
-var getInputs = "pacmd list-sink-inputs | awk '/index:/ {print $0};'";
+var getInputs = "pacmd list-sink-inputs | awk " + String.fromCharCode(39) + "/index:/ {print $0};" + String.fromCharCode(39);
 var wInputs = exec( getInputs , { silent:true , async: false }).stdout;
 wInputs = wInputs.split("\n");
 for ( var i = 0; i < wInputs.length; ++i ) {
@@ -10,9 +14,9 @@ for ( var i = 0; i < wInputs.length; ++i ) {
 	if ( x1.length == 0 ) { continue; }
 	var x2 = x1.split(":");
 	if ( !x2 ) { continue; }
-	if ( x2.length == 2 && x2[0] === 'index' ) {
+	if ( x2.length == 2 && x2[0] === "index" ) {
 		var wExec = "pacmd move-sink-input " + x2[1] + " 0";
 		exec( wExec , { silent:true , async: false } );
 	}
 }
-process.exit(1);
+process.exit(1);'
