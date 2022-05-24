90b44ede14d99a406d256990ce096bf586e3f36f
ceberous <cerbus.collin@gmail.com>
2022-05-24 15:06:46 -0400

    Update addUser.sh

--- addUser.sh
+++ addUser.sh
@@ -4,5 +4,7 @@ if [ ! -d "/home/$1" ]; then
         sudo mkdir -p /home/$1
         sudo chown -R $1:$1 /home/$1
 fi
+command=$(echo sudo su $1 -c "\"mkdir -p /home/$1/.ssh && touch /home/$1/.ssh/authorized_keys && chmod 600 /home/$1/.ssh/authorized_keys && chmod 700 /home/$1/.ssh && echo user created\"")
+/bin/bash -l -c "$command"
 # sudo usermod -aG sudo $1
 # sudo echo "${1} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
